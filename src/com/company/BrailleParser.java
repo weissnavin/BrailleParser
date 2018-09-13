8b6f6be69ddab91b4604a161a608e918ce5960dd
weissnavin <weissnavin@gmail.com>
2018-09-13 10:57:00 -0400

    Filled out the dictionary

--- src/com/company/BrailleParser.java
+++ src/com/company/BrailleParser.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Tuple {
     public final Boolean x;
@@ -16,6 +13,14 @@ class Tuple {
 }
 
 class Letter {
+    Tuple row1;
+    Tuple row2;
+    Tuple row3;
+    public Letter(Tuple row1, Tuple row2, Tuple row3){
+        this.row1=row1;
+        this.row2=row2;
+        this.row3=row3;
+    }
     List<Tuple> rep = Arrays.asList(new Tuple[3]);
     public Letter(List<Tuple> ls){
         this.rep=ls;
@@ -23,13 +28,96 @@ class Letter {
 }
 
 public class BrailleParser {
+    public static HashMap<Character, Letter> dict = new HashMap<>();
 
-        public List parser(String str){
+    public static void initialize(){
+        dict.put('A', Dictionary.A);
+        dict.put('B', Dictionary.B);
+        dict.put('C', Dictionary.C);
+        dict.put('D', Dictionary.D);
+        dict.put('E', Dictionary.E);
+        dict.put('F', Dictionary.F);
+        dict.put('G', Dictionary.G);
+        dict.put('H', Dictionary.H);
+        dict.put('I', Dictionary.I);
+        dict.put('J', Dictionary.J);
+        dict.put('K', Dictionary.K);
+        dict.put('L', Dictionary.L);
+        dict.put('M', Dictionary.M);
+        dict.put('N', Dictionary.N);
+        dict.put('O', Dictionary.O);
+        dict.put('P', Dictionary.P);
+        dict.put('Q', Dictionary.Q);
+        dict.put('R', Dictionary.R);
+        dict.put('S', Dictionary.S);
+        dict.put('T', Dictionary.T);
+        dict.put('U', Dictionary.U);
+        dict.put('V', Dictionary.V);
+        dict.put('W', Dictionary.W);
+        dict.put('X', Dictionary.X);
+        dict.put('Y', Dictionary.Y);
+        dict.put('Z', Dictionary.Z);
+        dict.put('#', Dictionary.HASH);
+        dict.put('1', Dictionary.ONE);
+        dict.put('2', Dictionary.TWO);
+        dict.put('3', Dictionary.THREE);
+        dict.put('4', Dictionary.FOUR);
+        dict.put('5', Dictionary.FIVE);
+        dict.put('6', Dictionary.SIX);
+        dict.put('7', Dictionary.SEVEN);
+        dict.put('8', Dictionary.EIGHT);
+        dict.put('9', Dictionary.NINE);
+        dict.put('0', Dictionary.ZERO);
+    }
+//    Dictionary brailleDict = new Dictionary();
 
-        return null;
+        public static void tupleToPrint(Tuple t){
+            if(t.x==true){
+                System.out.print('*');
+            }else{
+                System.out.print(' ');
+            }
+
+            if(t.y==true){
+                System.out.print("* ");
+            }else{
+                System.out.print("  ");
+            }
+        }
+
+        public static void parser(String str){
+            for (char c : str.toCharArray()){
+                Character up = Character.toUpperCase(c);
+                if(dict.containsKey(up)){
+                    tupleToPrint(dict.get(up).row1);
+                }
+            }
+            System.out.println();
+            for (char c : str.toCharArray()){
+                Character up = Character.toUpperCase(c);
+                if(dict.containsKey(up)){
+                    tupleToPrint(dict.get(up).row2);
+                }
+            }
+            System.out.println();
+            for (char c : str.toCharArray()){
+                Character up = Character.toUpperCase(c);
+                if(dict.containsKey(up)){
+                    tupleToPrint(dict.get(up).row3);
+                }
+            }
+            System.out.println();
+
     }
 
     public static void main(String[] args) {
+            initialize();
+        parser("ABCDEFGHIJKLM");
+        parser("NOPQRSTUVWXYZ");
+        parser("#1234567890");
+
+
         // write your code here
+
     }
 }
--- src/com/company/Dictionary.java
+++ src/com/company/Dictionary.java
@@ -2,13 +2,166 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class Dictionary {
+    public HashMap<Character, Letter> dict = new HashMap<>();
 
-    public static final Letter A = new Letter(Collections.unmodifiableList(
-            new ArrayList<Tuple>() {{
-                add(new Tuple(true, false));
-                add(new Tuple(false, false));
-                add(new Tuple(false, false));
-            }}));
+    public static final Letter A = new Letter(new Tuple(true, false),
+            new Tuple(false, false),
+            new Tuple(false, false));
+
+    public static final Letter B = new Letter(new Tuple(true, false),
+            new Tuple(true, false),
+            new Tuple(false, false));
+
+    public static final Letter C = new Letter(new Tuple(true, true),
+            new Tuple(false, false),
+            new Tuple(false, false));
+
+    public static final Letter D = new Letter(new Tuple(true, true),
+            new Tuple(false, true),
+            new Tuple(false, false));
+
+    public static final Letter E = new Letter(new Tuple(true, false),
+            new Tuple(false, true),
+            new Tuple(false, false));
+
+    public static final Letter F = new Letter(new Tuple(true, true),
+            new Tuple(true, false),
+            new Tuple(false, false));
+
+    public static final Letter G = new Letter(new Tuple(true, true),
+            new Tuple(true, true),
+            new Tuple(false, false));
+
+    public static final Letter H = new Letter(new Tuple(true, false),
+            new Tuple(true, true),
+            new Tuple(false, false));
+
+    public static final Letter I = new Letter(new Tuple(false, true),
+            new Tuple(true, false),
+            new Tuple(false, false));
+
+    public static final Letter J = new Letter(new Tuple(false, true),
+            new Tuple(true, true),
+            new Tuple(false, false));
+
+    public static final Letter K = new Letter(new Tuple(true, false),
+            new Tuple(false, false),
+            new Tuple(true, false));
+
+    public static final Letter L = new Letter(new Tuple(true, false),
+            new Tuple(true, false),
+            new Tuple(true, false));
+
+    public static final Letter M = new Letter(new Tuple(true, true),
+            new Tuple(false, false),
+            new Tuple(true, false));
+
+    public static final Letter N = new Letter(new Tuple(true, true),
+            new Tuple(false, true),
+            new Tuple(true, false));
+
+    public static final Letter O = new Letter(new Tuple(true, false),
+            new Tuple(false, true),
+            new Tuple(true, false));
+
+    public static final Letter P = new Letter(new Tuple(true, true),
+            new Tuple(true, false),
+            new Tuple(true, false));
+
+    public static final Letter Q = new Letter(new Tuple(true, true),
+            new Tuple(true, true),
+            new Tuple(true, false));
+
+    public static final Letter R = new Letter(new Tuple(true, false),
+            new Tuple(true, true),
+            new Tuple(true, false));
+
+    public static final Letter S = new Letter(new Tuple(false, true),
+            new Tuple(true, false),
+            new Tuple(true, false));
+
+    public static final Letter T = new Letter(new Tuple(false, true),
+            new Tuple(true, true),
+            new Tuple(true, false));
+
+    public static final Letter U = new Letter(new Tuple(true, false),
+            new Tuple(false, false),
+            new Tuple(true, true));
+
+    public static final Letter V = new Letter(new Tuple(true, false),
+            new Tuple(true, false),
+            new Tuple(true, true));
+
+    public static final Letter W = new Letter(new Tuple(false, true),
+            new Tuple(true, true),
+            new Tuple(false, true));
+
+    public static final Letter X = new Letter(new Tuple(true, true),
+            new Tuple(false, false),
+            new Tuple(true, true));
+
+    public static final Letter Y = new Letter(new Tuple(true, true),
+            new Tuple(false, true),
+            new Tuple(true, true));
+
+    public static final Letter Z = new Letter(new Tuple(true, false),
+            new Tuple(false, true),
+            new Tuple(true, true));
+
+    public static final Letter HASH = new Letter(new Tuple(false, true),
+            new Tuple(false, true),
+            new Tuple(true, true));
+
+    public static final Letter ONE = new Letter(new Tuple(true, false),
+            new Tuple(false, false),
+            new Tuple(false, false));
+
+    public static final Letter TWO = new Letter(new Tuple(true, false),
+            new Tuple(true, false),
+            new Tuple(false, false));
+
+    public static final Letter THREE = new Letter(new Tuple(true, true),
+            new Tuple(false, false),
+            new Tuple(false, false));
+
+    public static final Letter FOUR = new Letter(new Tuple(true, true),
+            new Tuple(false, true),
+            new Tuple(false, false));
+
+    public static final Letter FIVE = new Letter(new Tuple(true, false),
+            new Tuple(false, true),
+            new Tuple(false, false));
+
+    public static final Letter SIX = new Letter(new Tuple(true, true),
+            new Tuple(true, false),
+            new Tuple(false, false));
+
+    public static final Letter SEVEN = new Letter(new Tuple(true, true),
+            new Tuple(true, true),
+            new Tuple(false, false));
+
+    public static final Letter EIGHT = new Letter(new Tuple(true, false),
+            new Tuple(true, true),
+            new Tuple(false, false));
+
+    public static final Letter NINE = new Letter(new Tuple(false, true),
+            new Tuple(true, false),
+            new Tuple(false, false));
+
+    public static final Letter ZERO = new Letter(new Tuple(false, true),
+            new Tuple(true, true),
+            new Tuple(false, false));
+
+
+    public Dictionary(){
+        this.initialize();
+    }
+
+    public void initialize(){
+        dict.put('A', A);
+    }
+
 }
